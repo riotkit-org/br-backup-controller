18ec76b2b137402d954b9896aebfef087710d6e0
Andrew Johnson <blackandred@users.noreply.github.com>
2022-01-26 08:28:17 +0100

    tests: Cover Side docker somehow

--- test/test_transports_temporarydocker.py
+++ test/test_transports_temporarydocker.py
@@ -2,7 +2,7 @@ import os
 from io import StringIO
 
 from testcontainers.core.container import DockerContainer
-from testcontainers.core.waiting_utils import wait_for_logs
+from unittest.mock import patch
 
 from rkd.api.inputoutput import IO
 from rkd.api.testing import BasicTestingCase
@@ -18,34 +18,42 @@ class TestSideDockerTransport(BasicTestingCase):
 
     _nginx_container: DockerContainer
 
-    def setUp(self) -> None:
-        super().setUp()
-        self._nginx_container = DockerContainer(image='nginx:1.19-alpine').with_name("nginx-side-docker").start()
-
-    def tearDown(self) -> None:
-        super().tearDown()
-        self._nginx_container.stop(force=True, delete_volume=True)
-
-    # def test_captures_output(self):
-    #     self.assertIn(b'PG_VERSION=13', self._create_example_transport().capture('env').strip())
-
-    def test_transport_executes_command(self):
+    @patch('bahub.transports.docker.create_backup_maker_command')
+    def test_transport_executes_command_inside_temporary_container(self, create_backup_maker_command):
         transport = self._create_example_transport()
         definition = self._create_example_definition(transport)
 
-        with definition.transport(binaries=[]):
-            transport.schedule(
-                command="ls -la", definition=definition,
-                is_backup=True, version=""
-            )
+        create_backup_maker_command.return_value = ["ls", "-la", "/mnt"]
 
         io = IO()
         out = StringIO()
 
-        with io.capture_descriptors(stream=out, enable_standard_out=True):
-            transport.watch()
+        with definition.transport(binaries=[]):
+            transport.schedule(
+                command="--mocked--", definition=definition,
+                is_backup=True, version=""
+            )
 
-        print('!!!', out.getvalue())
+            with io.capture_descriptors(stream=out, enable_standard_out=True):
+                transport.watch()
+
+        # at /mnt was mounted ./.github directory from repository (see setUp())
+        self.assertIn('workflows', out.getvalue(),
+                      msg="Failed asserting that backup container on Alpine Linux has same volumes "
+                          "as application container with NGINX image")
+
+    # ================
+    # Technical stuff
+    # ================
+
+    def setUp(self) -> None:
+        super().setUp()
+        self._nginx_container = DockerContainer(image='quay.io/bitnami/nginx:1.21-debian-10')\
+            .with_name("nginx-side-docker").with_volume_mapping(os.getcwd() + "/.github", "/mnt").start()
+
+    def tearDown(self) -> None:
+        super().tearDown()
+        self._nginx_container.stop(force=True, delete_volume=True)
 
     @staticmethod
     def _create_example_definition(transport) -> FilesystemBackupDefinition:
@@ -65,7 +73,7 @@ class TestSideDockerTransport(BasicTestingCase):
                 "transport": transport
             },
             "spec": {
-                "paths": [os.getcwd() + "/.github"]
+                "paths": ["/app"]
             },
         }, name="fs")
 
@@ -74,8 +82,8 @@ class TestSideDockerTransport(BasicTestingCase):
         return Transport(
             spec={
                 'orig_container': "nginx-side-docker",
-                'temp_container_image': 'nginx:1.18-alpine',
-                'shell': '/bin/bash'
+                'temp_container_image': 'ghcr.io/mirrorshub/docker/alpine:3.14',
+                'shell': '/bin/sh'
             },
             io=IO()
         )
